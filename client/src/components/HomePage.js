6f198f666fcb5bc0cf5279381a4f4f1223a32ddf
dezrenmikell <dezren.mikell@yahoo.com>
2019-03-21 09:34:38 -0400

    https://mighty-woodland-78462.herokuapp.com

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -5,7 +5,7 @@ class HomePage extends Component {
     render() {
         return (
             <div>
-                <h1> Welcome to Plenty of Git</h1>
+                <h1> Welcome to Plenty of Git </h1>
             </div>
         );
     }

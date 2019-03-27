3f39a741f8328d6487540533c81d5b86e6a4d560
dezrenmikell <dezren.mikell@yahoo.com>
2019-03-27 10:12:35 -0400

    api displaying movies

--- client/src/components/MoviePage.js
+++ client/src/components/MoviePage.js
@@ -16,7 +16,9 @@ class MoviePage extends Component {
       .then(response => {
         const movies = response.data;
         this.setState({ savedMovies: movies });
-        console.log(movies);
+        console.log(movies[1].show.name);
+        console.log(movies[1].show.image.medium);
+        
       })
       .catch(err => {
         console.log("you messed up!", err);
@@ -26,14 +28,14 @@ class MoviePage extends Component {
     const savedMovies = this.state.savedMovies.map((movie, index)=>(
         <div key={index}>
         
-        <img src={movie.data}/>
+        <img src={movie.show.image.medium}/>
         </div>
     ));
     return (
       <div>
         <div className="savedMoviesContainer">
           <h3>movies</h3>
-          {/* <div className="savedMoviesFlexContainer">{savedMovies}</div> */}
+          <div >{savedMovies}</div>
         </div>
       </div>
     );
